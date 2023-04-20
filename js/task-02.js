814b8ff0984142a65cb9e2f4faa64274b98fa8ec
mepinit <evgenmep@gmail.com>
2023-04-20 22:48:05 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,61 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients");
+const liEl = document.createElement("li");
+// liEl.textContent = "Potatoes";
+// liEl.textContent = "Mushrooms";
+console.log(liEl.textContent += list[index])
+list.append(liEl);
+// liEl.forEach(el => {
+//   console.log(`${el.firstElementChild.textContent}`);
+// });
+// liEl.textContent = list(ingredients[index]);
+// liEl.classList.add("item");
+
+// const liEl1 = document.createElement("li");
+// liEl1.textContent = "Potatoes";
+// liEl1.classList.add("item");
+
+// const liEl2 = document.createElement("li");
+// liEl2.textContent = "Mushrooms";
+// liEl2.classList.add("item");
+
+// const liEl3 = document.createElement("li");
+// liEl3.textContent = "Garlic";
+// liEl3.classList.add("item");
+
+// const liEl4 = document.createElement("li");
+// liEl4.textContent = "Tomatos";
+// liEl4.classList.add("item");
+
+// const liEl5 = document.createElement("li");
+// liEl5.textContent = "Herbs";
+// liEl5.classList.add("item");
+
+// const liEl6 = document.createElement("li");
+// liEl6.textContent = "Condiments";
+// liEl6.classList.add("item");
+
+// list.appendChild(liEl1);
+
+console.log(liEl);
+// const liEl = document.createElement("li");
+// console.log(liEl);
+// liEl.classList.add("item");
+// console.log();
+// liEl.textContent = 'Potatoes';
+// liEl.textContent = 'Mushrooms';
+// liEl.textContent = 'Garlic';
+// liEl.textContent = 'Tomatos';
+// liEl.textContent = 'Herbs';
+// liEl.textContent = 'Condiments';
+// console.log(liEl);
+// ul#ingredients.appendChild(itemEl);
+// console.log();
+
+// const liIngredient = document.createElement("li");
+//   liIngredient.classList.add("item");
+//   liIngredient.textContent = ingredients[index];
+//   ulIngredients.appendChild(liIngredient);
