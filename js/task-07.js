f18774f6ac95be591ffb416e86a2e6b0aeb482df
mepinit <evgenmep@gmail.com>
2023-04-27 21:36:16 +0300

    7-9

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,14 @@
 // Завдання 7
-// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+// Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) 
+// і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті,
+//  перетягуючи повзунок, буде змінюватися розмір тексту.
 
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
 
-
-
-// textSize.style.fontSize = `${fontSizeControl.value}px`;
+const fontSizeControl = document.querySelector("#font-size-control");
+const textSize = document.querySelector("#text");
+fontSizeControl.addEventListener("input", (event)=> {    
+    textSize.style.fontSize = `${fontSizeControl.value}px`;
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,32 @@
+// Завдання 9
+// Напиши скрипт, який змінює кольори фону елемента <body> через
+//  інлайн-стиль по кліку на button.change-color і виводить значення кольору 
+//  в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const changeColorEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+changeColorEl.addEventListener("click", onChangeColor);
+function onChangeColor() {
+  const color = getRandomHexColor();
+  textColorEl.textContent = color;
+  document.body.style.backgroundColor = color;
 }
